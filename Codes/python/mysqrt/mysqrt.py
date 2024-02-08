c71a3eebfb1a0d69b31994773e2c2d6383a11610
shiv <shivanshudarkknight@gmail.com>
2024-02-08 20:01:17 +0530

    updated mysqrt.py as function

--- Codes/python/mysqrt/mysqrt.py
+++ Codes/python/mysqrt/mysqrt.py
@@ -1,12 +1,16 @@
-x= 2.0
-s= 1.0
-kmax= 20
-for k in range(kmax):
-    r=s
-    s= 0.5 *(s+ (x/s))
-    print(f"at iteration no. {k} is s={s}")
-    e=(s-r)/x
-    print(s)
-    if abs(e) < 1e-04:
-     break
-print(f"finally the value of s={s}")     
+
+tol=1.0e-14
+
+kmax=10000
+def mysqrt(x):
+    s=1.0
+    for k in range(kmax):
+        r=s
+        s= 0.5 *(s+ (x/s))
+        
+        e=(s-r)/x
+        e=abs(e)
+        
+        if abs(e) < 1e-14:
+           break
+        return s   
