0bbc113234ebfcbbf26384f0e3d2e78a1358a71f
shiv <shivanshudarkknight@gmail.com>
2024-02-10 23:53:17 +0530

    added docstring to mysqrt function

--- Codes/python/mysqrt/mysqrt.py
+++ Codes/python/mysqrt/mysqrt.py
@@ -1,16 +1,24 @@
+"""
+Module to calculate squareroot of a number
 
+
+"""
 tol=1.0e-14
 
-kmax=10000
-def mysqrt(x):
+kmax=100
+def mysqrt2(x):
+    """
+    Function to calculate squareroot of a number
+    """
+
     s=1.0
     for k in range(kmax):
         r=s
         s= 0.5 *(s+ (x/s))
-        
+         
         e=(s-r)/x
         e=abs(e)
         
         if abs(e) < 1e-14:
            break
-        return s   
+    return s   
